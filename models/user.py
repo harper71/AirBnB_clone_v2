9c86f181a63c2c9130c4386c1315f6a9f5ea9bbf
Abdul-Qudus <akindelequdus2@gmail.com>
2024-06-26 15:45:53 -0700

    new

--- models/user.py
+++ models/user.py
@@ -7,8 +7,13 @@ from models.base_model import Base
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    __tablename__ = 'users'
+    __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+
+    places = relationship("Place", cascade='all, delete, delete-orphan',
+                          backref="user")
+    reviews = relationship("Review", cascade='all, delete, delete-orphan',
+                           backref="user")
