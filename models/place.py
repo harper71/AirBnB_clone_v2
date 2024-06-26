33175dcb83e59aea8966ffd72c247de8fb81f462
Abdul-Qudus <akindelequdus2@gmail.com>
2024-06-26 15:32:29 -0700

    new

--- models/place.py
+++ models/place.py
@@ -7,14 +7,14 @@ from models.base_model import Base
 class Place(BaseModel):
     """ A place to stay """
     __tablename__ = 'places'
-    city_id = Custom(String(60), ForeignKey(cities.id), nullable=False)
-    user_id = Custom(String(60), ForeignKey(users.id), nullable=False)
-    name = Custom(String(128), nullable=False)
-    description = Custom(String(128), nullable=False)
-    number_rooms = Custom(Integer, nullable=False, default=0)
-    number_bathrooms = Custom(Integer, nullable=False, default=0)
-    max_guest = Custom(Integer, nullable=False, default=0)
-    price_by_night = Custom(Integer, nullable=False, default=0)
-    latitude = Custom(Float, nullable=True)
-    longitude = Custom(Float, nullable=True)
+    city_id = Column(String(60), ForeignKey(cities.id), nullable=False)
+    user_id = Column(String(60), ForeignKey(users.id), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(128), nullable=False)
+    number_rooms = Column(Integer, nullable=False, default=0)
+    number_bathrooms = Column(Integer, nullable=False, default=0)
+    max_guest = Column(Integer, nullable=False, default=0)
+    price_by_night = Column(Integer, nullable=False, default=0)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
     amenity_ids = []
